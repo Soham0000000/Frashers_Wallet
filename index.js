634681df1574fc0ad8b8bcdc545ddee2a65df388
Soham Osmani <sohamosmani@gmail.com>
2026-09-14 23:36:10 +0530

    Redirect root route to chats page

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ main()
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
-
+app.get("/",(req,res)=>{
+    res.redirect("/chats");
+});
 //Index Route
 app.get("/chats",async (req,res)=>{
     let chats = await Chat.find();
